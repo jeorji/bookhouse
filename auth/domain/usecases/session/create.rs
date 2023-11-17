db1f379fa1c0136a727751a875a4f79f56291101
jeorji <118723195+jeorji@users.noreply.github.com>
2023-11-17 18:01:16 +0700

    add session create use case

---
+++ auth/domain/usecases/session/create.rs
@@ -0,0 +1,59 @@
+use crate::entities;
+use crate::ports::{repositories::session, services::token_service};
+
+pub struct CreateUseCase {
+    session_repository: session::BxSessionRepository,
+    token_service: token_service::BxTokenService,
+}
+
+pub struct CreateSessionDTO {
+    pub user_id: uuid::Uuid,
+    pub user_group: i32,
+    /// ttl in seconds
+    pub refresh_token_ttl: u64,
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Repository error [{0}]")]
+    RepositoryError(#[from] crate::ports::Error),
+}
+
+impl CreateUseCase {
+    pub fn new(sr: session::BxSessionRepository, ts: token_service::BxTokenService) -> Self {
+        Self {
+            session_repository: sr,
+            token_service: ts,
+        }
+    }
+
+    pub async fn execute(&self, dto: CreateSessionDTO) -> Result<entities::Session, Error> {
+        let token_data = token_service::IssueTokenDTO {
+            user_id: dto.user_id,
+            permission_group: dto.user_group,
+        };
+        let token = self.token_service.issue(token_data)?;
+        let refresh_token_exp = current_timestamp() + dto.refresh_token_ttl;
+
+        let session_data = session::CreateSessionDTO {
+            user_id: dto.user_id,
+            refresh_token: token.refresh_token.clone(),
+            refresh_token_exp,
+        };
+
+        self.session_repository.create(session_data).await?;
+
+        Ok(entities::Session::new(
+            dto.user_id,
+            token.refresh_token,
+            refresh_token_exp,
+        ))
+    }
+}
+
+fn current_timestamp() -> u64 {
+    std::time::SystemTime::now()
+        .duration_since(std::time::UNIX_EPOCH)
+        .expect("time went backwards")
+        .as_secs() as u64
+}
