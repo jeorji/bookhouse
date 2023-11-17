1727b3e0c2bd8e7ffc9710d8dead29fe06d1e2a9
jeorji <118723195+jeorji@users.noreply.github.com>
2023-11-17 19:27:39 +0700

    remove unused code in repository ports

--- auth/domain/ports/repositories/permission.rs
+++ auth/domain/ports/repositories/permission.rs
@@ -3,17 +3,11 @@ use crate::ports::Error;
 #[async_trait::async_trait]
 pub trait PermissionRepository {
     async fn find(&self, dto: FindPermissionDTO) -> Result<Option<PermissionDTO>, Error>;
-    async fn create(&self, dto: CreatePermissionDTO) -> Result<PermissionDTO, Error>;
 }
 pub type BxPermissionRepository = Box<dyn PermissionRepository>;
 
 pub struct FindPermissionDTO {
     pub user_id: uuid::Uuid,
-}
-
-pub struct CreatePermissionDTO {
-    pub user_id: uuid::Uuid,
-    pub group: i32,
 }
 
 pub struct PermissionDTO {
--- auth/domain/ports/repositories/session.rs
+++ auth/domain/ports/repositories/session.rs
@@ -2,23 +2,19 @@ use crate::ports::Error;
 
 #[async_trait::async_trait]
 pub trait SessionRepository {
-    async fn find(&self, dto: FindSessionDTO) -> Result<Option<SessionDTO>, Error>;
-    async fn update(&self, dto: UpdateSessionDTO) -> Result<bool, Error>;
     async fn create(&self, dto: CreateSessionDTO) -> Result<SessionDTO, Error>;
 }
 pub type BxSessionRepository = Box<dyn SessionRepository>;
 
-pub struct FindSessionDTO {
-    pub refresh_token: String,
-}
-
-pub struct UpdateSessionDTO {
-    pub new_refresh_token: String,
-}
-
 pub struct CreateSessionDTO {
     pub user_id: uuid::Uuid,
     pub refresh_token: String,
+    pub refresh_token_exp: u64,
 }
 
-pub struct SessionDTO {}
+pub struct SessionDTO {
+    pub id: uuid::Uuid,
+    pub user_id: uuid::Uuid,
+    pub refresh_token: String,
+    pub refresh_token_exp: u64,
+}
